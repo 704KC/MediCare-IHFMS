e4144756278cc4d19b5d9a299aeb163cb62d4b11
Nakiboneka Mary <nakibonekamarymargret@gmail.com>
2024-04-11 16:39:54 +0300

    Resolved merge conflicts

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //echo "MAN FROM NOWHERE is the guy who does software engineering ";
-    $value=6;
-    $name="AMADILE";
-    $value=($value*6);
-    echo "$name <br>";
-    //echo 'Hello='.$value;
-    echo '<a href="https://www.facebook.com">MAN FROM NOWHERE</a>';
-?>
